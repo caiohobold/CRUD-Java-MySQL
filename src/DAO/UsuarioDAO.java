3275f95aeae59e9bfa803d692daf64d4fe11f81d
caiohobold <caiomarconhobold21@gmail.com>
2024-03-13 15:03:17 -0300

    Inserção da função de Update de cadastros

--- src/DAO/UsuarioDAO.java
+++ src/DAO/UsuarioDAO.java
@@ -63,6 +63,43 @@ public class UsuarioDAO {
 
     }
 
+    public Usuario alteraUsuario(int id){
+        Usuario usuario = null;
+        String sql = "SELECT * FROM USUARIO WHERE IDUSUARIO=?";
+
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql)) {
+            ps.setInt(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    usuario = new Usuario();
+                    usuario.setCodigo(rs.getInt("IDUSUARIO"));
+                    usuario.setNome(rs.getString("NOME"));
+                    usuario.setLogin(rs.getString("LOGIN"));
+                    usuario.setSenha(rs.getString("SENHA"));
+                    usuario.setEmail(rs.getString("EMAIL"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+
+    public void atualizarUsuario(Usuario usuario) {
+        String sql = "UPDATE USUARIO SET NOME=?, LOGIN=?, SENHA=?, EMAIL=? WHERE IDUSUARIO=?";
+    
+        try (PreparedStatement ps = Conexao.getConexao().prepareStatement(sql)) {
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getLogin());
+            ps.setString(3, usuario.getSenha());
+            ps.setString(4, usuario.getEmail());
+            ps.setInt(5, usuario.getCodigo());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class UserInterface extends JFrame implements ActionListener {
-    JButton cadastrarButton, procurarButton, removerButton;
+    JButton cadastrarButton, procurarButton, removerButton, alterarButton, alterarNome, alterarLogin, alterarSenha, alterarEmail;
     UsuarioDAO usuarioDAO;
 
     public UserInterface() {
@@ -37,6 +37,10 @@ public class UserInterface extends JFrame implements ActionListener {
         procurarButton.setPreferredSize(new Dimension(350, 50));
         procurarButton.addActionListener(this);
 
+        alterarButton = new JButton("Alterar Usuário");
+        alterarButton.setPreferredSize(new Dimension(350, 50));
+        alterarButton.addActionListener(this);
+
         removerButton = new JButton("Remover Usuário");
         removerButton.setBackground(Color.RED);
         removerButton.setForeground(Color.WHITE); 
@@ -45,6 +49,7 @@ public class UserInterface extends JFrame implements ActionListener {
 
         add(cadastrarButton);
         add(procurarButton);
+        add(alterarButton);
         add(removerButton);
 
         setVisible(true);
@@ -62,6 +67,8 @@ public class UserInterface extends JFrame implements ActionListener {
             procurarUsuarioPorId();
         } else if (e.getSource() == removerButton) {
             removerUsuario();
+        } else if (e.getSource() == alterarButton){
+            alterarUsuario();
         }
     }
 
@@ -108,4 +115,73 @@ public class UserInterface extends JFrame implements ActionListener {
             JOptionPane.showMessageDialog(this, "Nenhum usuário encontrado com o ID: " + id);
         }
     }
+
+    private void alterarUsuario(){
+        int id = Integer.parseInt(JOptionPane.showInputDialog(this, "Digite o ID do usuário:"));
+        Usuario usuario = usuarioDAO.alteraUsuario(id);
+        if (usuario != null) {
+            JPanel panel = new JPanel();
+            panel.setLayout(new GridLayout(0, 1));
+
+            panel.add(new JLabel("ID: " + usuario.getCodigo()));
+            panel.add(new JLabel("Nome: " + usuario.getNome()));
+            panel.add(new JLabel("Login: " + usuario.getLogin()));
+            panel.add(new JLabel("Senha: " + usuario.getSenha()));
+            panel.add(new JLabel("Email: " + usuario.getEmail()));
+
+            alterarNome = new JButton("Alterar Nome");
+            alterarNome.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    String novoNome = JOptionPane.showInputDialog(UserInterface.this, "Digite o novo nome:");
+                    if(novoNome != null){
+                        usuario.setNome(novoNome);
+                        usuarioDAO.atualizarUsuario(usuario);
+                        JOptionPane.showMessageDialog(UserInterface.this, "Nome alterado com sucesso!");
+                    }
+                }
+            });
+            alterarLogin = new JButton("Alterar Login");
+            alterarLogin.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    String novoLogin = JOptionPane.showInputDialog(UserInterface.this, "Digite o novo login:");
+                    if(novoLogin != null){
+                        usuario.setLogin(novoLogin);
+                        usuarioDAO.atualizarUsuario(usuario);
+                        JOptionPane.showMessageDialog(UserInterface.this, "Login alterado com sucesso!");
+                    }
+                }
+            });
+            alterarSenha = new JButton("Alterar Senha");
+            alterarSenha.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    String novaSenha = JOptionPane.showInputDialog(UserInterface.this, "Digite a nova senha:");
+                    if(novaSenha != null){
+                        usuario.setSenha(novaSenha);
+                        usuarioDAO.atualizarUsuario(usuario);
+                        JOptionPane.showMessageDialog(UserInterface.this, "Senha alterada com sucesso!");
+                    }
+                }
+            });
+            alterarEmail = new JButton("Alterar E-mail");
+            alterarEmail.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e){
+                    String novoEmail = JOptionPane.showInputDialog(UserInterface.this, "Digite o novo email:");
+                    if(novoEmail != null){
+                        usuario.setEmail(novoEmail);
+                        usuarioDAO.atualizarUsuario(usuario);
+                        JOptionPane.showMessageDialog(UserInterface.this, "Email alterado com sucesso!");
+                    }
+                }
+            });
+            
+            panel.add(alterarNome);
+            panel.add(alterarLogin);
+            panel.add(alterarSenha);
+            panel.add(alterarEmail);
+            JOptionPane.showMessageDialog(this, panel, "Informações do Usuário", JOptionPane.PLAIN_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Nenhum usuário encontrado com o ID: " + id);
+        }
+    }
+
 }
